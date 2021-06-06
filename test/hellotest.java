992ee922aed78188c8e5a1efd480fc83d03da447
Prathmesh Garud <prathmesh.garud@cloudreach.com>
2021-06-06 13:31:36 +0530

    adinng test

--- test/hellotest.java
+++ test/hellotest.java
@@ -12,7 +12,11 @@ public class hellotest {
 
     @Test
     public void testWillAlwaysFail() {
+<<<<<<< HEAD
         fail("This is An Error for demo/testing purpose");
+=======
+        fail("This is An Error for testing git & jenkins pipeline");
+>>>>>>> 04b679ef699f72ec51c4f0f27d26dd442a489ffb
     }
 
 
